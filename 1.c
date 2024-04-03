98eb5ac76445090eace91f6a3591881aadebfca1
imlinux68 <imlinux68@gmail.com>
2024-04-03 20:25:51 +0300

    improved 1.c

--- 1.c
+++ 1.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
-#include<conio.h>
 
-void main()
-{
-	clrscr(); /* Clears Screen */
-	printf("Hello, World!");
-	getch();  /* Holds the output */
-	return 0;
+int main() {
+    printf("Hello, World!\n");
+    getchar();
+    return 0;
 }
